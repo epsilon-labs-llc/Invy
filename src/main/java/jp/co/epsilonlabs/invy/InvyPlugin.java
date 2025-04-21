474cac331dd1c01a10ab4587c4db2f8d1451dfb5
tomgly <tomtom.s1412@gmail.com>
2025-04-20 21:50:36 -0400

    Initial commit

---
+++ src/main/java/jp/co/epsilonlabs/invy/InvyPlugin.java
@@ -0,0 +1,17 @@
+package jp.co.epsilonlabs.invy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InvyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
